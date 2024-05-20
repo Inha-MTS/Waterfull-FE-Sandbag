f9d7b4984cd11a2ebaa78fd7f099be17e2b3abd4
caputdraconis <caputdraconis@kakao.com>
2024-05-20 16:13:16 +0900

    feat: sending image per 2 second to lambda

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useRef } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import './App.css';
+import axios from 'axios';
 
 function Login() {
   const videoRef = useRef(null);
-  const [searchParams] = useSearchParams();
-  const how = searchParams.get('how'); // 'face' or 'card'
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     async function enableStream() {
@@ -28,12 +26,42 @@ function Login() {
     };
   }, []);
 
+  useEffect(() => {
+    let imageCount = 0; // Add this line to track the number of images sent
+  
+    const sendImageToApi = () => {
+      const interval = setInterval(() => {
+        if (videoRef.current && canvasRef.current) {
+          const canvas = canvasRef.current;
+          const context = canvas.getContext('2d');
+          context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+          canvas.toBlob(async (blob) => {
+            const formData = new FormData();
+            formData.append('image', blob);
+            formData.append('fileName', `${++imageCount}.jpg`); // Update this line to use the imageCount
+            try {
+              await axios.post('https://p2pvgx6eak.execute-api.ap-northeast-2.amazonaws.com/dev/image', formData, {
+                headers: {
+                  'Content-Type': 'multipart/form-data'
+                }
+              });
+            } catch (err) {
+              console.error('Error sending the image:', err);
+            }
+          });
+        }
+      }, 2000);
+  
+      return () => clearInterval(interval);
+    };
+  
+    sendImageToApi();
+  }, []);
+
   return (
     <div style={{ position: 'relative' }}>
       <video ref={videoRef} autoPlay playsInline/>
-      {how === 'face' && <img src="guide-img/face-guide.png" id="guideImage" alt="Face Guide" />}
-      {how === 'card' && <img src="guide-img/card-guide.png" id="guideImage" alt="Card Guide" />}
-
+      <canvas ref={canvasRef} style={{ display: 'none' }} width={640} height={480} />
     </div>
   );
 }
