7d6ebbce09c5a054ea69d28e742ad8411f6972b1
caputdraconis <caputdraconis@kakao.com>
2024-06-04 10:34:49 +0900

    chore: organize route

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './pages/Home';
 import Login from './Login';
+import UserButton from './component/UserButton';
+import Register from './pages/Register';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/test-page"
+        element={
+          <UserButton
+            buttonText="테스트"
+            outlineColor="black"
+            onClick={() => {
+              alert('test');
+            }}
+          />
+        }
+      />
     </Routes>
   );
 };
