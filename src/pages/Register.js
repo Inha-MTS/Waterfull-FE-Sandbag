16bf11b7c885625a2d843d2ae6d48a5c7b75d8e3
caputdraconis <caputdraconis@kakao.com>
2024-06-04 16:06:37 +0900

    chore: use UserInput for user input field

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,26 +7,41 @@ import UserButton from '../component/UserButton';
 import UserInput from '../component/UserInput';
 
 const questions = [
-  { mainText: "이름을 입력해주세요", subText: "리워드 지급을 위해서 꼭 본명을 입력해주세요", input: true },
-  { mainText: "학번을 입력해주세요", subText: "8자리의 학번을 입력해주세요", input: true },
-  { mainText: "학과를 입력해주세요", subText: "학과명을 줄이지 말고 입력해주세요", input: true },
-  { mainText: "얼굴을 등록하시겠어요?", subText: "얼굴을 등록하면 리워드를 받을 수 있습니다", input: false },
+  {
+    mainText: '이름을 입력해주세요',
+    subText: '리워드 지급을 위해서 꼭 본명을 입력해주세요',
+    input: true,
+  },
+  {
+    mainText: '학번을 입력해주세요',
+    subText: '8자리의 학번을 입력해주세요',
+    input: true,
+  },
+  {
+    mainText: '학과를 입력해주세요',
+    subText: '학과명을 줄이지 말고 입력해주세요',
+    input: true,
+  },
+  {
+    mainText: '얼굴을 등록하시겠어요?',
+    subText: '얼굴을 등록하면 리워드를 받을 수 있습니다',
+    input: false,
+  },
 ];
 
 function Register() {
-    const [index, setIndex] = useState(0);
-    const [inputValue, setInputValue] = useState('');
+  const [index, setIndex] = useState(0);
+  const [inputValue, setInputValue] = useState('');
 
-    return (
-        <div className="App">
-        <header className="App-header">
-        
+  return (
+    <div className="App">
+      <header className="App-header">
         <MainText text="이름을 입력해주세요" />
         <SubText text="리워드 지급을 위해서 꼭 본명을 입력해주세요" />
-        <UserInput />
-        </header>
-        </div>
-    );
+        <UserInput placeholder="김인덕" />
+      </header>
+    </div>
+  );
 }
 
 export default Register;
