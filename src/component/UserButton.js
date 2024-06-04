6166341ee549e4e0149d519df3c44eb3ee161603
caputdraconis <caputdraconis@kakao.com>
2024-06-04 15:50:45 +0900

    style: Organize Buttonx2 to Component

--- src/component/UserButton.js
+++ src/component/UserButton.js
@@ -1,14 +1,13 @@
-import './UserButton.css';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
-function UserButton({ buttonText, outlineColor, onClick }) {
+function UserButton({firstText, firstOnClick, secondText, secondOnClick}) {
   return (
-    <div 
-      className="user-button" 
-      onClick={onClick}
-      style={{ border: `1px solid ${outlineColor}` }}
-    >
-      {buttonText}
-    </div>
+    <Stack gap={2} className="col-md-5 mx-auto">
+      <Button variant="primary" size="lg" onClick={firstOnClick} style={{height: '100px', fontSize: '50px'}}>{firstText}</Button>
+      <div style={{display: 'block', marginTop: '40px'}}></div>
+      <Button variant="success" size="lg" onClick={secondOnClick} style={{height: '100px', fontSize: '50px'}}>{secondText}</Button>
+    </Stack>
   );
 }
 
