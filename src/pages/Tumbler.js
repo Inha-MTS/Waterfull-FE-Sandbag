6374ebf02748af42b8e3fd2295211694f127fb4f
caputdraconis <caputdraconis@kakao.com>
2024-06-05 10:40:44 +0900

    feat: add page tumbler ready

--- src/pages/Tumbler.js
+++ src/pages/Tumbler.js
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import MainText from '../component/MainText';
 import SubText from '../component/SubText';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function Tumbler() {
   const navigate = useNavigate();
+  const query = useQuery();
+  const name = query.get('name');
+  const studentId = query.get('studentId');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/tumbler-rek?name=' + name + '&studentId=' + studentId);
+    }, 3000);
+
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머를 제거
+  }, [navigate]);
+
   return (
     <div className="App">
       <header className="App-header">
