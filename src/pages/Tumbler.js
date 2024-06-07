c90b9b1bf7341eae92108373821e81bba54fa56f
caputdraconis <caputdraconis@kakao.com>
2024-06-07 19:45:16 +0900

    chore: support multi lang

--- src/pages/Tumbler.js
+++ src/pages/Tumbler.js
@@ -9,15 +9,15 @@ function useQuery() {
 
 const messages = {
   kr: {
-    main: '텀블러 인식을 시작할게요',
-    sub: '텀블러를 준비해주세요',
+    main: '안녕하세요 ',
+    sub: '텀블러 인식을 시작할게요',
   },
   en: {
-    main: "Let's start recognizing Tumblr",
-    sub: 'Please prepare Tumblr',
+    main: 'Hi ',
+    sub: 'Getting ready for Tumblr',
   },
   cn: {
-    main: '让我们开始认识 Tumblr',
+    main: '你好 ',
     sub: '请准备好 Tumblr',
   },
 };
@@ -31,7 +31,14 @@ function Tumbler() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/tumbler-rek?name=' + name + '&studentId=' + studentId);
+      navigate(
+        '/tumbler-rek?name=' +
+          name +
+          '&studentId=' +
+          studentId +
+          '&lang=' +
+          lang,
+      );
     }, 3000);
 
     return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머를 제거
@@ -40,7 +47,9 @@ function Tumbler() {
   return (
     <div className="App">
       <header className="App-header">
-        <MainText text={messages[lang]['main']} />
+        <MainText
+          text={messages[lang]['main'] + name + (lang === 'kr' ? '님' : '')}
+        />
         <SubText text={messages[lang]['sub']} />
       </header>
     </div>
